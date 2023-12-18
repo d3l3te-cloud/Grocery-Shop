0bf39df3066acb5b2401c0034a75aad049918384
Toiletdestroyer94 <154253554+Toiletdestroyer94@users.noreply.github.com>
2023-12-18 22:06:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 print('Hello World')
 print('why are you gay') 
 print('mai nhi hu') 
+D={}
 print(''' instructions
 1. press 1 for purchase
 2. press 2 for selling
@@ -8,4 +9,7 @@ print(''' instructions
 4. press 4 to exit the system''') 
 while true:
   x=int(input('enter command:'))
+  if x==1:
+    
+
   
