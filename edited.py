7bf79aa0bab0d7a18c50879b8326f3f02c8602dc
d3l3te-cloud <154224835+d3l3te-cloud@users.noreply.github.com>
2023-12-24 23:50:01 +0530

    Update edited.py

--- edited.py
+++ edited.py
@@ -1,62 +1,37 @@
 #editedfilehere
 T = {}
-bdgt= int(input("enter the budget of the store(NUMERIC INPUT):"))
-bdgt1=bdgt
+#Creating the item code in loop
+a=10000
+c=0
+
 while True:
-    a = int(input(" 1 to add items, 2 to re purchase , 3 to sell,4 to delete, 5 to display and 6 to exit:"))
-    if a == 1:
-        itm = input("Enter the item")
-        qunty= int(input("Enter the quantity of te item:"))
-        amt= int(input("Enter the price of  a single item:"))
-        b = qunty*amt
-        bdgt1-=b
-        print ("Amount left in the store =", bdgt1)
-        T[itm]= {"quanity":qunty,"amount" : b}
-        c= qunty
-        f= b
-        print("Item purchased")
-    if a == 2:
-        itm = input("Enter the itm")
-        if itm in T:
-            qunty= int(input("Enter the quantity of the item:"))
-            amt= int(input("Enter the price of  a single item:"))
-            b = qunty*amt
-            bdgt1-=b
-            print ("Amount left in the store == ", bdgt1)
-            c+= qunty
-            f-=b
-            T[itm]= {"quanity":c,"Profit/Loss" : f}
-            print("Item purchased")
-        else:
-            print("Item not found")
-    if a == 3:
-        itm = input("Enter the itm")
-        if itm in T:
-            qunty = int(input("Enter the quantity of the item:"))
-            amt = int(input("Enter the  sell price of  a single ietm:"))
-            b = qunty*amt
-            bdgt1+=b
-            print ("Amount left in the store == ", bdgt1)
-            c-= qunty
-            f+=b
-            T[itm]= {"quanity":c,"Profit/Loss" : f}
-            print("Item sold")
-        else:
-            print("Item not found")
-    if a == 4:
-        itm = input("Enter the item")
-        if itm in T:
-            del T[itm]
-            print("Item Deleted")
-    if a == 5:
-        f= int(input("Enter 1 to see a particular item otherwise press any other number:"))
-        if f== 1:
-            itm = input("Enter the item:")
-            if itm in T:
-                print(T[itm])
-        elif a != 1:
-            print(T)
-        print ("Amount left in the store == ", bdgt1)
-    if a == 6:
-        print("Thanks for using...exited")
-        break
+    choice= int(input(" 1 for administration, 2 for billing, 3 to close store:"))
+    if choice == 1:
+        while True:
+            a = int(input(" 1 to add stocks,2 to display and 3 to exit administration:"))
+            if a==1:
+                Itms= input("Enter the item")
+                qunty= int(input("Enter the quantity of te item:"))
+                amt= int(input("Enter the price of  a single item:"))
+                total= qunty*amt
+                if Itms not in T:
+                    itmcode=a
+                    a+=c
+                    c+=1
+                print("Itemcode:",itmcode)
+                T[itmcode]= {"Name":Itms,"Quantity":qunty}
+                 
+            if a == 2:
+                f= int(input("Enter 1 to see a particular item otherwise press any other number:"))
+                if f== 1:
+                    itm = int(input("Enter the itemcode:")
+                if itm in T:
+                    print(T[itm])
+                elif a != 1:
+                    print(T)
+            if a ==3:
+                print("Exiting administration:")
+                break
+        
+
+    if choice == 2:
